d9f190322b49b8dc5df13857360b1283cb72423b
ben-rieth <benrieth3@gmail.com>
2022-08-31 15:26:39 -0500

    Get jots on page

--- pages/jots/index.tsx
+++ pages/jots/index.tsx
@@ -1,7 +1,6 @@
 import DateHeader from "components/DateHeader";
 import JotList from "components/JotList";
 import NewJotForm from "components/NewJotForm";
-import { Type } from '@prisma/client';
 import { FormikValues } from "formik";
 import axios from "axios";
 import useSWR from 'swr';
@@ -10,27 +9,23 @@ const fetcher = ( url:string ) => axios.get(url).then(res => res.data)
 
 const JotPage = () => {
 
-    const { data, error } = useSWR('/api/jots', fetcher);
+    const { data, error } = useSWR('/api/jots', fetcher, {refreshInterval: 1000});
 
     const addJot = async (values: FormikValues) => {
         await axios.post('/api/jots', values)
     }
 
     return (
-        <>
-            <DateHeader />
-            <JotList jots={[
-                {
-                    id: 'id',
-                    content: 'This is a jot',
-                    type: Type.TASK,
-                    important: false,
-                    createdAt: new Date(),
-                    updatedAt: new Date()
-                }
-            ]}/>
-            <NewJotForm onSubmit={addJot}/>
-        </>
+        !error ? (
+            <>
+                <DateHeader />
+                <JotList jots={data}/>
+                <NewJotForm onSubmit={addJot}/>
+            </>
+        ): (
+            <p>Could not connect to server</p>
+        )
+        
     )
 }
 
