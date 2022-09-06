8f7f744f5fc6b87aaed9c4038fd4c6afa3dcdbcd
ben-rieth <benrieth3@gmail.com>
2022-09-06 17:43:19 -0500

    Create header with logo

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,16 @@
 import Head from "next/head";
-import { FC } from "react";
+import { FC, Fragment } from "react";
+import { IoJournal } from "react-icons/io5";
+import { Menu, Transition } from '@headlessui/react';
 
 type LayoutProps = {
     children: JSX.Element
 }
 
 const Layout : FC<LayoutProps> = ({ children }) => {
+
+    const user = false;
+
     return (
         <>
             <Head>
@@ -16,6 +21,15 @@ const Layout : FC<LayoutProps> = ({ children }) => {
                 />
                 <link rel="icon" href="/favicon.io" />
             </Head>
+
+            <header className="flex items-center justify-center w-full h-12 px-2 shadow-md">
+                <div className="flex items-center justify-center">
+                    <IoJournal className="w-7 h-7 fill-sky-500"/>
+                    <h1 className="text-3xl italic font-bold text-sky-500">
+                        BuJots
+                    </h1>
+                </div>
+            </header>
 
             <main className="container flex-grow mx-auto">
                 {children}
