01663e8cd513f55636e439f2dc53c5b18cd8c7b7
ben-rieth <benrieth3@gmail.com>
2022-09-17 14:29:56 -0500

    Update prisma schema with users

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from 'lib/prisma';
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        
+    ]
+})
