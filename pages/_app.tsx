a67ed85a50ba16b746e78920dc74dfe20c86bc6e
ben-rieth <benrieth3@gmail.com>
2022-09-17 14:42:49 -0500

    Set up EmailProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
 import Layout from 'components/layout'
+import {SessionProvider as AuthProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: {session, ...pageProps} }: AppProps) {
 
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <AuthProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
       <Toaster position="bottom-center"/>
     </>
   )
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,23 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from 'lib/prisma';
+import EmailProvider from 'next-auth/providers/email';
+import nodemailer from 'nodemailer';
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
-        
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_HOST,
+                port: Number(process.env.EMAIL_PORT),
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                }
+            },
+            from: process.env.EMAIL_FROM,
+            maxAge: 10 * 60
+        })
     ]
 })
