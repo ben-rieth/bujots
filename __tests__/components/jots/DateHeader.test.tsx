9a5bcca129d1067bfa7b41f90b22ed018bb5fe78
ben-rieth <benrieth3@gmail.com>
2022-09-29 16:11:16 -0500

    Update DateHeader to have left and right arrows

--- __tests__/components/jots/DateHeader.test.tsx
+++ __tests__/components/jots/DateHeader.test.tsx
@@ -31,4 +31,14 @@ describe('Testing DateHeader Component', () => {
 
         expect(header.textContent).toBe("December 17, 1995")
     });
+
+    it('renders a left and right arrow', () => {
+        render(<DateHeader />);
+
+        const left = screen.getByTestId('left');
+        const right = screen.getByTestId('right');
+
+        expect(left).toBeInTheDocument();
+        expect(right).toBeInTheDocument();
+    })
 })
--- components/jots/DateHeader.tsx
+++ components/jots/DateHeader.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
+import { IoMdArrowForward, IoMdArrowBack} from 'react-icons/io';
 
 type DateHeaderProps = {
     date?: Date
+    onBackClick?: () => void
+    onForwardClick?: () => void
 }
 
-const DateHeader : FC<DateHeaderProps> = ({date = new Date()}) => {
+const DateHeader : FC<DateHeaderProps> = ({date = new Date(), onBackClick, onForwardClick}) => {
 
     const dateString = date.toLocaleDateString(
         undefined,
@@ -16,9 +19,22 @@ const DateHeader : FC<DateHeaderProps> = ({date = new Date()}) => {
     )
 
     return (
-        <h1 className="text-xl font-bold text-center">
-            {dateString}
-        </h1>
+        <div className="flex items-center justify-center gap-3 pt-2">
+            <IoMdArrowBack 
+                data-testid="left" 
+                className="fill-sky-500 w-6 h-6"
+                onClick={onBackClick}
+            />
+            <h1 className="text-xl font-bold text-center">
+                {dateString}
+            </h1>
+            <IoMdArrowForward 
+                data-testid="right" 
+                className="fill-sky-500 w-6 h-6"
+                onClick={onForwardClick}
+            />
+        </div>
+        
         
     )
 }
