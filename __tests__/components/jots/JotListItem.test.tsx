1925a0a581ffe91d17e846e3ea1bb3fbb73d63dc
ben-rieth <benrieth3@gmail.com>
2022-10-06 17:28:06 -0500

    Recreate Jot Component without interactions

--- __tests__/components/jots/JotListItem.test.tsx
+++ __tests__/components/jots/JotListItem.test.tsx
@@ -7,6 +7,17 @@ import { Jot, Type, Status } from '@prisma/client';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
+const testJot : Jot = {
+    id: 'id',
+    content: 'This is a jot',
+    status: Status.ACTIVE,
+    type: Type.NOTE,
+    important: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    date: new Date()
+}
+
 const server = setupServer(
     rest.patch('/api/jots/id', (req, res, ctx) => {
         return res(
@@ -20,7 +31,45 @@ beforeAll(() => server.listen());
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers())
 
-describe("Testing Jot Component", () => {
+describe("Testing JotListItem Component", () => {
+    it('displays the text content of the jot', () => {
+        render(<JotListItem jot={testJot} isToday={true}/>);
+
+        const content = screen.getByText('This is a jot');
+
+        expect(content).toBeInTheDocument();
+    });
+
+    it('displays a dash if the jot is a NOTE', () => {
+        testJot.type = Type.NOTE;
+
+        render(<JotListItem jot={testJot} isToday={true}/>);
+
+        const icon = screen.getByTestId('dash');
+        expect(icon).toBeInTheDocument();
+
+    });
+
+    it('displays a triangle if the jot is an EVENT', () => {
+        testJot.type = Type.EVENT;
+
+        render(<JotListItem jot={testJot} isToday={true}/>)
+
+        const icon = screen.getByTestId('triangle-outline');
+        expect(icon).toBeInTheDocument();
+    });
+
+    it('displays a circle if the jot is a TASK', () => {
+        testJot.type = Type.TASK;
+
+        render(<JotListItem jot={testJot} isToday={true}/>)
+
+        const icon = screen.getByTestId('circle-outline');
+        expect(icon).toBeInTheDocument();
+    });
+})
+
+describe.skip("Testing Old Jot Component", () => {
 
     const testJot : Jot = {
         id: 'id',
--- components/jots/JotListItem.tsx
+++ components/jots/JotListItem.tsx
@@ -11,91 +11,111 @@ type JotListItemProps = {
     isToday: boolean
 }
 
-const JotListItem : FC<JotListItemProps> = ({jot, isToday=false }) => {
-    const { mutate } = useSWRConfig();
-
-    const [formVisible, setFormVisible] = useState<boolean>(false);
-    const [internalJot, setInternalJot] = useState<Jot>(jot);
-
-    const [migrated, setMigrated] = useState<boolean>(false);
-
-    const openForm = () => {
-        if(isToday) {
-            setFormVisible(true)
-        }
-    };
-
-    const closeForm = () => setFormVisible(false);
-
-    const updateJot = async (values: Partial<Jot>) => {
-
-        const oldJotValues = internalJot;
-        setInternalJot({...internalJot, ...values});
-        
-        await axios.patch(`/api/jots/${jot.id}`, values)
-            .catch((error) => {
-                setInternalJot(oldJotValues)
-                throw new Error(error);
-            });
-        mutate('/api/jots')
-    }
-
-    const toggleComplete = async () => {
-        if(!isToday) return
-        if (internalJot.status === Status.DELETED) return;
-
-        const newStatus = internalJot.status === Status.COMPLETED ? Status.ACTIVE : Status.COMPLETED;
-
-        setInternalJot({
-            ...internalJot,
-            status: newStatus
-        });
-
-        await axios.patch(`/api/jots/${jot.id}`, { status: newStatus });
-        mutate('/api/jots')
-    }
-
-    if (migrated) {
-        return null;
-    }
-
+const JotListItem:FC<JotListItemProps> = ({ jot, isToday=false}) => {
     return (
-        !formVisible ? (
-            <article className="flex items-center gap-1 py-1" >
-                <JotIcon jot={internalJot} onClick={toggleComplete} />
+        <article className="flex items-center gap-1 py-1">
+            <JotIcon jot={jot} onClick={() => {}} />
                 <span 
                     className={`
                         flex-auto 
-                        ${internalJot.status === Status.COMPLETED && "text-green-500"}
-                        ${internalJot.status === Status.DELETED && "line-through"}
-                        ${(internalJot.important && internalJot.status !== "DELETED") 
+                        ${jot.status === Status.COMPLETED && "text-green-500"}
+                        ${jot.status === Status.DELETED && "line-through"}
+                        ${(jot.important && jot.status !== "DELETED") 
                             && "underline decoration-rose-500 underline-offset-2 decoration-2 font-bold"}
                     `}
-                    onClick={openForm}
                     data-testid="content"
                 >
-                    {internalJot.content}
+                    {jot.content}
                 </span>
-
-
-                <MigrateIcon 
-                    jot={internalJot} 
-                    isToday={isToday}
-                    onMigrate={() => setMigrated(true)} />
-            </article>
-        ) : (
-            <JotForm 
-                onSubmit={updateJot}
-                done={closeForm}
-                initialValues={{
-                    content: internalJot.content,
-                    status: internalJot.status,
-                    important: internalJot.important,
-                    type: internalJot.type
-                }}
-            />
-        )
+        </article>
     )
 }
+
+// const JotListItem : FC<JotListItemProps> = ({jot, isToday=false }) => {
+//     const { mutate } = useSWRConfig();
+
+//     const [formVisible, setFormVisible] = useState<boolean>(false);
+//     const [internalJot, setInternalJot] = useState<Jot>(jot);
+
+//     const [migrated, setMigrated] = useState<boolean>(false);
+
+//     const openForm = () => {
+//         if(isToday) {
+//             setFormVisible(true)
+//         }
+//     };
+
+//     const closeForm = () => setFormVisible(false);
+
+//     const updateJot = async (values: Partial<Jot>) => {
+
+//         const oldJotValues = internalJot;
+//         setInternalJot({...internalJot, ...values});
+        
+//         await axios.patch(`/api/jots/${jot.id}`, values)
+//             .catch((error) => {
+//                 setInternalJot(oldJotValues)
+//                 throw new Error(error);
+//             });
+//         mutate('/api/jots')
+//     }
+
+//     const toggleComplete = async () => {
+//         if(!isToday) return
+//         if (internalJot.status === Status.DELETED) return;
+
+//         const newStatus = internalJot.status === Status.COMPLETED ? Status.ACTIVE : Status.COMPLETED;
+
+//         setInternalJot({
+//             ...internalJot,
+//             status: newStatus
+//         });
+
+//         await axios.patch(`/api/jots/${jot.id}`, { status: newStatus });
+//         mutate('/api/jots')
+//     }
+
+//     if (migrated) {
+//         return null;
+//     }
+
+//     return (
+//         !formVisible ? (
+//             <article className="flex items-center gap-1 py-1" >
+//                 <JotIcon jot={internalJot} onClick={toggleComplete} />
+//                 <span 
+//                     className={`
+//                         flex-auto 
+//                         ${internalJot.status === Status.COMPLETED && "text-green-500"}
+//                         ${internalJot.status === Status.DELETED && "line-through"}
+//                         ${(internalJot.important && internalJot.status !== "DELETED") 
+//                             && "underline decoration-rose-500 underline-offset-2 decoration-2 font-bold"}
+//                     `}
+//                     onClick={openForm}
+//                     data-testid="content"
+//                 >
+//                     {internalJot.content}
+//                 </span>
+
+
+//                 <MigrateIcon 
+//                     jot={internalJot} 
+//                     isToday={isToday}
+//                     onMigrate={() => setMigrated(true)} />
+//             </article>
+//         ) : (
+//             <JotForm 
+//                 onSubmit={updateJot}
+//                 done={closeForm}
+//                 initialValues={{
+//                     content: internalJot.content,
+//                     status: internalJot.status,
+//                     important: internalJot.important,
+//                     type: internalJot.type
+//                 }}
+//             />
+//         )
+//     )
+// }
 
 export default JotListItem;
